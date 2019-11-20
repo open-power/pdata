47bba0cac3a618bbbae76b9e0d55d8dbf41e6026
Amitay Isaacs <amitay@ozlabs.org>
2019-11-20 16:49:54 +1100

    attribute: Remove atdb based attribute tool
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

--- tools/translate.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TRANSLATE_H
-#define __TRANSLATE_H
-
-#include <libpdbg.h>
-
-struct pdbg_target *from_cronus_target(const char *name);
-char *to_cronus_target(struct pdbg_target *target);
-
-#endif /* __TRANSLATE_H */
