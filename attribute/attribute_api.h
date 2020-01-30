8e7fafb826434887ca6bfc89c63bd435f1d80fbf
Amitay Isaacs <amitay@ozlabs.org>
2020-01-30 17:36:27 +1100

    attribute_api: Document attribute read/write api
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

--- attribute/attribute_api.h
+++ attribute/attribute_api.h
@@ -17,10 +17,59 @@
 #ifndef __ATTRIBUTE_API_H__
 #define __ATTRIBUTE_API_H__
 
+/**
+ * @file attribute_api.h
+ * @author Amitay Isaacs
+ * @brief Attribute API to read/write attributes from device tree
+ *
+ * The attribute api provides accessing attributes stored in device tree as
+ * properties.  The attributes are encoded to store the meta-data along with
+ * the actual value of the attribute.  This allows correctly handling data
+ * endian.
+ *
+ * The attribute api is based on libpdbg library.
+ *
+ */
 #include <libpdbg.h>
 
+/**
+ * @brief Read an attribute from device tree
+ *
+ * The calling program needs to know the actual size of the attribute value.
+ * If there is a mismatch between the expected size and the actual stored
+ * size, then error is returned.  The value of attribute is copied to the
+ * provided buffer.
+ *
+ * @param[in] target  Pdbg target identifying device tree node
+ * @param[in] name   Name of the attribute
+ * @param[out] value  Buffer for value of the attribute
+ * @param[in] value_len  Length of the value of the attribute
+ * @return 0 on success, errno on failure
+ *
+ * ENOENT   if attribute is not found
+ * EMSGSIZE if the value_len does not match the length of value stored
+ * EINVAL   if the attribute value is not initialized
+ *
+ */
 int attr_read(struct pdbg_target *target, const char *name,
 	      uint8_t *value, size_t value_len);
+
+/**
+ * @brief Write an attribute to device tree
+ *
+ * If there is a mismatch between the value size and the actual stored
+ * size, then error is returned.
+ *
+ * @param[in] target  Pdbg target identifying device tree node
+ * @param[in] name   Name of the attribute
+ * @param[in] value  Value of the attribute
+ * @param[in] value_len  Length of the value of the attribute
+ * @return 0 on success, errno on failure
+ *
+ * ENOENT   if attribute is not found
+ * EMSGSIZE if the value_len does not match the length of value stored
+ * EIO      if there was an error writing to device tree
+ */
 int attr_write(struct pdbg_target *target, const char *name,
 	       uint8_t *value, size_t value_len);
 
