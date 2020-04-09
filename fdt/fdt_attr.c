bd66ef7cd46212f5c1b0ee1846a4b5c1bbd39e6c
Amitay Isaacs <amitay@ozlabs.org>
2020-04-09 15:03:43 +1000

    fdt: Add fdt based attribute api (libfdt-attr)
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

---
+++ fdt/fdt_attr.c
@@ -0,0 +1,274 @@
+/* Copyright 2020 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <libfdt.h>
+
+#include "fdt_prop.h"
+
+int fdt_attr_read(void *fdt, const char *path, const char *name,
+		  uint32_t data_size, uint32_t count, uint8_t *value)
+{
+	uint8_t *buf;
+	int buflen, ret, i;
+
+	if (data_size > 8 || count > 1000000)
+		return EINVAL;
+
+	buflen = data_size * count;
+	buf = malloc(buflen);
+	if (!buf)
+		return ENOMEM;
+
+	ret = fdt_prop_read(fdt, path, name, buf, &buflen);
+	if (!ret) {
+		free(buf);
+		return ret;
+	}
+
+	if (data_size == 1) {
+		memcpy(value, buf, buflen);
+
+	} else if (data_size == 2) {
+		uint16_t *b = (uint16_t *)buf;
+		uint16_t *v = (uint16_t *)value;
+
+		for (i=0; i<count; i++)
+			v[i] = be16toh(b[i]);
+
+	} else if (data_size == 4) {
+		uint32_t *b = (uint32_t *)buf;
+		uint32_t *v = (uint32_t *)value;
+
+		for (i=0; i<count; i++)
+			v[i] = be32toh(b[i]);
+
+	} else if (data_size == 8) {
+		uint64_t *b = (uint64_t *)buf;
+		uint64_t *v = (uint64_t *)value;
+
+		for (i=0; i<count; i++)
+			v[i] = be64toh(b[i]);
+
+	} else {
+		free(buf);
+		return EINVAL;
+	}
+
+	free(buf);
+	return 0;
+}
+
+int fdt_attr_write(void *fdt, const char *path, const char *name,
+		   uint32_t data_size, uint32_t count, uint8_t *value)
+{
+	uint8_t *buf;
+	int buflen, ret, i;
+
+	if (data_size > 8 || count > 1000000)
+		return EINVAL;
+
+	buflen = data_size * count;
+	buf = malloc(buflen);
+	if (!buf)
+		return ENOMEM;
+
+	if (data_size == 1) {
+		memcpy(buf, value, buflen);
+
+	} else if (data_size == 2) {
+		uint16_t *b = (uint16_t *)buf;
+		uint16_t *v = (uint16_t *)value;
+
+		for (i=0; i<count; i++)
+			b[i] = htobe16(v[i]);
+
+	} else if (data_size == 4) {
+		uint32_t *b = (uint32_t *)buf;
+		uint32_t *v = (uint32_t *)value;
+
+		for (i=0; i<count; i++)
+			b[i] = htobe32(v[i]);
+
+	} else if (data_size == 8) {
+		uint64_t *b = (uint64_t *)buf;
+		uint64_t *v = (uint64_t *)value;
+
+		for (i=0; i<count; i++)
+			b[i] = htobe64(v[i]);
+
+	} else {
+		free(buf);
+		return EINVAL;
+	}
+
+	ret = fdt_prop_write(fdt, path, name, buf, buflen);
+	if (ret != 0) {
+		free(buf);
+		return ret;
+	}
+
+	free(buf);
+	return 0;
+}
+
+static int spec_size(const char *spec)
+{
+	int size = 0, i;
+
+	for (i=0; i<strlen(spec); i++) {
+		char ch = spec[i];
+
+		if (ch == '1')
+			size += 1;
+		else if (ch == '2')
+			size += 2;
+		else if (ch == '4')
+			size += 4;
+		else if (ch == '8')
+			size += 8;
+		else
+			return -1;
+	}
+
+	return size;
+}
+
+int fdt_attr_read_packed(void *fdt, const char *path, const char *name,
+			 const char *spec, uint8_t *value)
+{
+	uint8_t *buf;
+	int buflen, ret, pos, i;
+
+	if (!spec || spec[0] == '\0')
+		return EINVAL;
+
+	buflen = spec_size(spec);
+	if (buflen <= 0)
+		return EINVAL;
+
+	buf = malloc(buflen);
+	if (!buf)
+		return ENOMEM;
+
+	ret = fdt_prop_read(fdt, path, name, buf, &buflen);
+	if (ret != 0) {
+		free(buf);
+		return ret;
+	}
+
+	pos = 0;
+	for (i=0; i<strlen(spec); i++) {
+		char ch = spec[i];
+
+		if (ch == '1') {
+			uint8_t *b = buf + pos;
+			uint8_t *v = value + pos;
+
+			*v = *b;
+			pos += 1;
+
+		} else if (ch == '2') {
+			uint16_t *b = (uint16_t *)(buf + pos);
+			uint16_t *v = (uint16_t *)(value + pos);
+
+			*v = be16toh(*b);
+			pos += 2;
+
+		} else if (ch == '4') {
+			uint32_t *b = (uint32_t *)(buf + pos);
+			uint32_t *v = (uint32_t *)(value + pos);
+
+			*v = be32toh(*b);
+			pos += 4;
+
+		} else if (ch == '8') {
+			uint64_t *b = (uint64_t *)(buf + pos);
+			uint64_t *v = (uint64_t *)(value + pos);
+
+			*v = be64toh(*b);
+			pos += 8;
+		}
+	}
+
+	free(buf);
+	return 0;
+}
+
+int fdt_attr_write_packed(void *fdt, const char *path, const char *name,
+			  const char *spec, uint8_t *value)
+{
+	uint8_t *buf;
+	int buflen, ret, pos, i;
+
+	if (!spec || spec[0] == '\0')
+		return EINVAL;
+
+	buflen = spec_size(spec);
+	if (buflen <= 0)
+		return EINVAL;
+
+	buf = malloc(buflen);
+	if (!buf)
+		return ENOMEM;
+
+	pos = 0;
+	for (i=0; i<strlen(spec); i++) {
+		char ch = spec[i];
+
+		if (ch == '1') {
+			uint8_t *b = buf + pos;
+			uint8_t *v = value + pos;
+
+			*b = *v;
+			pos += 1;
+
+		} else if (ch == '2') {
+			uint16_t *b = (uint16_t *)(buf + pos);
+			uint16_t *v = (uint16_t *)(value + pos);
+
+			*b = htobe16(*v);
+			pos += 2;
+
+		} else if (ch == '4') {
+			uint32_t *b = (uint32_t *)(buf + pos);
+			uint32_t *v = (uint32_t *)(value + pos);
+
+			*b = htobe32(*v);
+			pos += 4;
+
+		} else if (ch == '8') {
+			uint64_t *b = (uint64_t *)(buf + pos);
+			uint64_t *v = (uint64_t *)(value + pos);
+
+			*b = htobe64(*v);
+			pos += 8;
+		}
+	}
+
+	ret = fdt_prop_write(fdt, path, name, buf, buflen);
+	if (ret != 0) {
+		free(buf);
+		return ret;
+	}
+
+	free(buf);
+	return 0;
+}
---
+++ fdt/fdt_attr.h
@@ -0,0 +1,125 @@
+/* Copyright 2020 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __FDT_ATTR_H__
+#define __FDT_ATTR_H__
+
+/**
+ * @file fdt_attr.h
+ * @brief FDT based attribute api
+ *
+ * libfdt_attr defines attribute handling API where attributes are stored as
+ * properties in device trees.  Attributes values have specific data
+ * structure.  Two types of data structures are supported:
+ *
+ * 1. simple (arrays of 1, 2, 4, or 8 byte integers)
+ * 2. complex (packed stream of 1, 2, 4, or 8 byte integers)
+ *
+ * The write APIs will ensure that data is stored in consistent endian in the
+ * device tree.  The read APIs will ensure that data is retrieved in host
+ * endian format.
+ */
+
+/**
+ * @brief Read simple value of given attribute from device tree
+ *
+ * The attribute value is considered as an array of 1, 2, 4 or 8 byte integer.
+ *
+ * @param[in] fdt Flatted device tree pointer
+ * @param[in] path Full path of the node in device tree
+ * @param[in] name Name of the attribute
+ * @param[in] data_size Size of an element in the array
+ * @param[in] count Number of elements in the array
+ * @param[out] value Value of the attribute
+ * @return 0 on success, errno on failure
+ *    ENOENT - If the node or attribute does not exist
+ *    ENOMEM - If memory allocaton fails
+ *    EINVAL - If the data_size and/or count are invalid
+ *    EIO    - If there is error reading attribute
+ */
+int fdt_attr_read(void *fdt, const char *path, const char *name,
+		  uint32_t data_size, uint32_t count, uint8_t *value);
+
+/**
+ * @brief Write simple value of given attribute to device tree
+ *
+ * The attribute value is considered as an array of 1, 2, 4 or 8 byte integer.
+ *
+ * @param[in] fdt Flatted device tree pointer
+ * @param[in] path Full path of the node in device tree
+ * @param[in] name Name of the attribute
+ * @param[in] data_size Size of an element in the array
+ * @param[in] count Number of elements in the array
+ * @param[out] value Value of the attribute
+ * @return 0 on success, errno on failure
+ *    ENOENT - If the node or attribute does not exist
+ *    ENOMEM - If memory allocaton fails
+ *    EINVAL - If the data_size and/or count are invalid
+ *    EIO    - If there is error writing attribute
+ */
+int fdt_attr_write(void *fdt, const char *path, const char *name,
+		   uint32_t data_size, uint32_t count, uint8_t *value);
+
+/**
+ * @brief Read complex value of given attribute from device tree
+ *
+ * The attribute value is considered as a packed stream of 1, 2, 4 or 8 byte
+ * integers.  The specification describes how the integers are packed.
+ *
+ * Example:
+ *    A stream of uint32_t, uint8_t, uint16_t, uint32_t is specified as
+ *    "4124".
+ *
+ * @param[in] fdt Flatted device tree pointer
+ * @param[in] path Full path of the node in device tree
+ * @param[in] name Name of the attribute
+ * @param[in] spec Specification of packed integers
+ * @param[out] value Value of the attribute
+ * @return 0 on success, errno on failure
+ *    ENOENT - If the node or attribute does not exist
+ *    ENOMEM - If memory allocaton fails
+ *    EINVAL - If the specification is invalid
+ *    EIO    - If there is error reading attribute
+ */
+int fdt_attr_read_packed(void *fdt, const char *path, const char *name,
+			 const char *spec, uint8_t *value);
+
+/**
+ * @brief Write complex value of given attribute to device tree
+ *
+ * The attribute value is considered as a packed stream of 1, 2, 4 or 8 byte
+ * integers.  The specification describes how the integers are packed.
+ *
+ * Example:
+ *    A stream of uint32_t, uint8_t, uint16_t, uint32_t is specified as
+ *    "4124".
+ *
+ * @param[in] fdt Flatted device tree pointer
+ * @param[in] path Full path of the node in device tree
+ * @param[in] name Name of the attribute
+ * @param[in] spec Specification of packed integers
+ * @param[out] value Value of the attribute
+ * @return 0 on success, errno on failure
+ *    ENOENT - If the node or attribute does not exist
+ *    ENOMEM - If memory allocaton fails
+ *    EINVAL - If the specification is invalid
+ *    EIO    - If there is error writing attribute
+ */
+int fdt_attr_write_packed(void *fdt, const char *path, const char *name,
+			  const char *spec, uint8_t *value);
+
+#endif /* __FDT_ATTR_H__ */
+
