412707e6cf3791b902ce703ce0720dec25e5ca7a
Ramesh Iyyar <rameshi1@in.ibm.com>
2020-08-19 06:54:21 -0500

    scripts: Reordered the steps to generate header|infodb|dts
    - header
      - It required only attributes definition.
    
    - infodb
      - It required attributes definition and attributes mapping info
        into required targets i.e currently infodb does not keep
        attributes values so, system specific details is not required.
    
    - dts
      - It required all details i.e attributes definition, attributes mapping
        info into required targets and system specific attributes value.
    
    Also, created "tmp" directory for respective filetype to keep generated
    xml files in all those steps.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

--- scripts/dt_generate.sh
+++ scripts/dt_generate.sh
@@ -42,12 +42,6 @@ debug ()
     fi
 }
 
-# Ensure required environment variables are defined
-check_var SYSTEM_NAME
-check_var TARGET_PROC
-check_var SYSTEM_MRW_XML
-check_var EKB
-
 if [ $# -ne 2 ] ; then
     usage
 fi
@@ -59,8 +53,16 @@ fi
 filetype="$1"
 outfile="$2"
 
+# Ensure required environment variables are defined
+check_var TARGET_PROC
+check_var EKB
+if [ "$filetype" = "dts" ] ; then
+    check_var SYSTEM_NAME
+    check_var SYSTEM_MRW_XML
+fi
+
 out_dir=$(dirname "$outfile")
-tmp_dir="$out_dir/tmp"
+tmp_dir="$out_dir/tmp_$filetype"
 rm -rf "$tmp_dir"
 mkdir -p "$tmp_dir"
 
@@ -76,35 +78,12 @@ filter_target="$data_dir/filter_TargetsList.lsv"
 export PERL5LIB="$script_dir"
 
 # Step 1:
-#      Processing system xml to get all required targets and attributes
-#      for device tree hierarchy and platform specific system
-#      targets processing
-
-debug "Step 1: Processing system xml " $(basename "$SYSTEM_MRW_XML")
-"$script_dir/processMrw.pl" \
-    -x "$SYSTEM_MRW_XML" \
-    -b bmc \
-    -o "$tmp_dir/${SYSTEM_NAME}_bmc_mrw.xml"
-
-# Step 2:
-#      Filtering system mrw xml which is generated from step1
-#      by using system specific filter file
-
-debug "Step 2: Filtering processed system xml " $(basename "$SYSTEM_MRW_XML")
-"$script_dir/filterXML.pl" \
-    --inXML "$tmp_dir/${SYSTEM_NAME}_bmc_mrw.xml" \
-    --outXML "$tmp_dir/${SYSTEM_NAME}_bmc_mrw_filtered.xml" \
-    --filterAttrsFile "$filter_attr" \
-    --filterTgtsFile "$filter_target" \
-    --filterType systemXML
-
-# Step 3:
 #      Converting required ekb attributes xml into mrw attributes and targets
 #      type xml format, because ekb and mrw attributes xml files are
 #      different format, so it will be difficult to process xml's to
 #      generate device tree blob file. So splitting ekb xml
 
-debug "Step 3: Splitting EKB HWPs required attributes xml into MRW Targets and Attributes type xml"
+debug "Step 1: Splitting EKB HWPs required attributes xml into MRW Targets and Attributes type xml"
 "$script_dir/processEkbAttrsXML.pl" \
     --inEkbAttrsXMLPath ${EKB} \
     --inEkbAttrsXMLFilesList "$data_dir/reqEkbAttrsXmlFileList.lsv" \
@@ -113,101 +92,128 @@ debug "Step 3: Splitting EKB HWPs required attributes xml into MRW Targets and A
     --outEkbAttrsXML "$tmp_dir/attribute_types_ekb.xml" \
     --outEkbTgtsXML "$tmp_dir/target_types_ekb.xml"
 
-# Step 4:
+# Step 2:
 #      Merging FAPI and Non-FAPI attributes type xml into single
 #      xml file for making process simple
 
-debug "Step 4: Merging FAPI and Non-FAPI attributes definition..."
+debug "Step 2: Merging FAPI and Non-FAPI attributes definition..."
 "$script_dir/mergeMRWFormatXml.sh" \
     "$tmp_dir/attribute_types_ekb.xml" \
     "$data_dir/attribute_types_obmc.xml" \
     > "$tmp_dir/attribute_types_final.xml"
 
-# Step 5:
-#      Mapping custom targets parent attributes into platform xml file
-#      for it child targets
-
-debug "Step 5: Mapping custom targets parent attributes into platform xml file"
-"$script_dir/mapTgtsAttrs.pl" \
-    --fromTgtXml "$data_dir/target_types_obmc.xml" \
-    --filterTgtList "$filter_target" \
-    --outXml "$tmp_dir/target_types_obmc_withItsParentAttrs.xml" \
-    --tgtXMLType customTgt
-
-# Step 6:
-#      Mapping generated ekb targets attributes into platform xml file
-
-debug "Step 6: Mapping FAPI Targets along with attributes into bmc plat file"
-"$script_dir/mapTgtsAttrs.pl" \
-    --fromTgtXml "$tmp_dir/target_types_ekb.xml" \
-    --toTgtXml "$tmp_dir/target_types_obmc_withItsParentAttrs.xml" \
-    --outXml "$tmp_dir/target_types_final.xml" \
-    --tgtXMLType ekbTgt
-
 if [ "$filetype" = "header" ] ; then
 
-# Step 7: Generating header file from intermediate xml which is contains
-#         MRW, FAPI and Non-FAPI targets/attributes.
-#
-#         The generated header file will contains meta data i.e
-#          - data type: to declare attribute variable
-#          - macros: to set/get attributes value from device tree
-#          - spec: for api purpose to take care endianess
-#          - element count: attribute value element count
+    # Step 3: Generating header file from intermediate xml which is contains
+    #         MRW, FAPI and Non-FAPI targets/attributes.
+    #
+    #         The generated header file will contains meta data i.e
+    #          - data type: to declare attribute variable
+    #          - macros: to set/get attributes value from device tree
+    #          - spec: for api purpose to take care endianess
+    #          - element count: attribute value element count
 
-    debug "Step 7: Generating header file"
+    debug "Step 3: Generating header file"
     "$script_dir/genAttrsHeaderFile.pl" \
         --inXML "$tmp_dir/attribute_types_final.xml" \
         --outAHFile "$outfile"
 
-elif [ "$filetype" = "infodb" ] ; then
+else # infodb | dts
 
-# Step 7:
-#      Merging all following generated xml files into single for
-#      creating infodb intermediate xml file
-#            - target_types_final.xml
-#            - attribute_types_final.xml
+    # Step 3:
+    #      Mapping custom targets parent attributes into platform xml file
+    #      for it child targets
 
-    debug "Step 7: Getting infodb intermediate xml by merging target and attributes types xml files"
-    "$script_dir/mergeMRWFormatXml.sh" \
-        "$tmp_dir/target_types_final.xml" \
-        "$tmp_dir/attribute_types_final.xml" \
-        > "$tmp_dir/infodb_intermediate.xml"
+    debug "Step 3: Mapping custom targets parent attributes into platform xml file"
+    "$script_dir/mapTgtsAttrs.pl" \
+        --fromTgtXml "$data_dir/target_types_obmc.xml" \
+        --filterTgtList "$filter_target" \
+        --outXml "$tmp_dir/target_types_obmc_withItsParentAttrs.xml" \
+        --tgtXMLType customTgt
 
-# Step 8: Generating attributes info database which is contains
-#         MRW, FAPI and Non-FAPI targets/attributes and generated info db
-#         can use for attribute tool to read|write attributes by using
-#         power system device tree.
+    # Step 4:
+    #      Mapping generated ekb targets attributes into platform xml file
 
-    debug "Step 8: Generating info db file"
-    "$script_dir/genAttrsInfoDB.pl" \
-        --inXML "$tmp_dir/infodb_intermediate.xml" \
-        --outInfoDB "$outfile"
+    debug "Step 4: Mapping FAPI Targets along with attributes into bmc plat file"
+    "$script_dir/mapTgtsAttrs.pl" \
+        --fromTgtXml "$tmp_dir/target_types_ekb.xml" \
+        --toTgtXml "$tmp_dir/target_types_obmc_withItsParentAttrs.xml" \
+        --outXml "$tmp_dir/target_types_final.xml" \
+        --tgtXMLType ekbTgt
 
-else
 
-# Step 7:
-#      Merging all following generated xml files into single for
-#      creating intermediate xml file
-#            - System xml
-#            - target_types_final.xml
-#            - attribute_types_final.xml
+    if [ "$filetype" = "infodb" ] ; then
 
-    debug "Step 7: Getting intermediate xml by merging system, target and attributes types xml files"
-    "$script_dir/mergeMRWFormatXml.sh" \
-        "$tmp_dir/${SYSTEM_NAME}_bmc_mrw_filtered.xml" \
-        "$tmp_dir/target_types_final.xml" \
-        "$tmp_dir/attribute_types_final.xml" \
-        > "$tmp_dir/intermediate.xml"
+        # Step 5:
+        #      Merging all following generated xml files into single for
+        #      creating infodb intermediate xml file
+        #            - target_types_final.xml
+        #            - attribute_types_final.xml
 
-# Step 8: Generating device tree structure (dts) file to generate DTB
-#
-#         The generated dts file will contain targets and its attributes as a
-#         device tree format, so dtc compiler can use to get dtb file
+        debug "Step 5: Getting infodb intermediate xml by merging target and attributes types xml files"
+        "$script_dir/mergeMRWFormatXml.sh" \
+            "$tmp_dir/target_types_final.xml" \
+            "$tmp_dir/attribute_types_final.xml" \
+            > "$tmp_dir/infodb_intermediate.xml"
 
-    debug "Step 8: Generating device tree structure file"
-    "$script_dir/genDTS.pl" \
-        --inXML "$tmp_dir/intermediate.xml" \
-        --pdbgMapFile "$data_dir/pdbg_compatible_propMapping.lsv" \
-        --outDTS "$outfile"
-fi
+        # Step 6: Generating attributes info database which is contains
+        #         MRW, FAPI and Non-FAPI targets/attributes and generated info db
+        #         can use for attribute tool to read|write attributes by using
+        #         power system device tree.
+
+        debug "Step 6: Generating info db file"
+        "$script_dir/genAttrsInfoDB.pl" \
+            --inXML "$tmp_dir/infodb_intermediate.xml" \
+            --outInfoDB "$outfile"
+
+    else # dts
+
+        # Step 5:
+        #      Processing system xml to get all required targets and attributes
+        #      for device tree hierarchy and platform specific system
+        #      targets processing
+
+        debug "Step 5: Processing system xml " $(basename "$SYSTEM_MRW_XML")
+        "$script_dir/processMrw.pl" \
+            -x "$SYSTEM_MRW_XML" \
+            -b bmc \
+            -o "$tmp_dir/${SYSTEM_NAME}_bmc_mrw.xml"
+
+        # Step 6:
+        #      Filtering system mrw xml which is generated from step1
+        #      by using system specific filter file
+
+        debug "Step 6: Filtering processed system xml " $(basename "$SYSTEM_MRW_XML")
+        "$script_dir/filterXML.pl" \
+            --inXML "$tmp_dir/${SYSTEM_NAME}_bmc_mrw.xml" \
+            --outXML "$tmp_dir/${SYSTEM_NAME}_bmc_mrw_filtered.xml" \
+            --filterAttrsFile "$filter_attr" \
+            --filterTgtsFile "$filter_target" \
+            --filterType systemXML
+
+        # Step 7:
+        #      Merging all following generated xml files into single for
+        #      creating intermediate xml file
+        #            - System xml
+        #            - target_types_final.xml
+        #            - attribute_types_final.xml
+
+        debug "Step 7: Getting intermediate xml by merging system, target and attributes types xml files"
+        "$script_dir/mergeMRWFormatXml.sh" \
+            "$tmp_dir/${SYSTEM_NAME}_bmc_mrw_filtered.xml" \
+            "$tmp_dir/target_types_final.xml" \
+            "$tmp_dir/attribute_types_final.xml" \
+            > "$tmp_dir/intermediate.xml"
+
+        # Step 8: Generating device tree structure (dts) file to generate DTB
+        #
+        #         The generated dts file will contain targets and its attributes as a
+        #         device tree format, so dtc compiler can use to get dtb file
+
+        debug "Step 8: Generating device tree structure file"
+        "$script_dir/genDTS.pl" \
+            --inXML "$tmp_dir/intermediate.xml" \
+            --pdbgMapFile "$data_dir/pdbg_compatible_propMapping.lsv" \
+            --outDTS "$outfile"
+    fi # infodb | dts
+fi # header | [ infodb | dts ]
